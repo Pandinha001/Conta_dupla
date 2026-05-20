8c789afa17ee9fd9e2ed9b7bdb0a30fd7177e69f
Luizzzzzzz <luiztavinho241@gmail.com>
2026-05-20 16:53:04 -0300

    Update main.py
    main da conta

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("oi mundo")
+print("1 - sacar")
+print("2 - depositar")
+print("3 - calcular_rend")
+
+dinheiro = Conta(100)
+
+escolha = int(input('o que deseja fazer?'))
+if escolha == 1:
+  dinheiro.sacar(50)
+elif escolha == 2:
+  dinheiro.depositar(20)
+elif escolha == 3:
+  dinheiro.calcular_rend()
+  
+  
+  
+
+
+
+

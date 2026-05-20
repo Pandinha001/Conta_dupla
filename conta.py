f7001a983e64ccd3f48bc849c55726e165ebafec
Aline <aline09ribeirao@gmail.com>
2026-05-20 16:42:25 -0300

    Update2 conta.py
    Classe Conta

--- conta.py
+++ conta.py
@@ -1,1 +1,17 @@
-print("olá mundo")
+class Conta:
+  def __init__(self, saldo):
+    self.saldo = saldo
+  def sacar(self, valor):
+    self.saldo -= valor
+    print(f' seu saldo atual é: {self.saldo}')
+  def depositar(self, valor):
+    self.saldo += valor
+    print(f' seu saldo atual é: {self.saldo}')
+  def calcular_rend(self):
+    rend = self.saldo * 0.1
+    print(f"Seu rendimento foi: {rend}")
+
+dinheiro = Conta(100)
+dinheiro.sacar(50)
+dinheiro.depositar(20)
+dinheiro.calcular_rend()
